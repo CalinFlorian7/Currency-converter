12b58d0f93d4a9a0edb657ea4b7e1daa70c6c6d4
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-27 15:58:08 +0300

    verifing the api key existence

--- src/main/java/com/example/currencyconverter/currency/CurrencyConverter.java
+++ src/main/java/com/example/currencyconverter/currency/CurrencyConverter.java
@@ -1,11 +1,39 @@
 package com.example.currencyconverter.currency;
 
+
+import com.example.currencyconverter.dotenv.DotenvLoader;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class CurrencyConverter {
     private String fromCurrency;
     private String toCurrency;
     private double amount;
     private double convertedAmount;
     private CurrencyCodes currencyCodes;
+    private double conversionRate;
+
+    private String conversionResult;
+    public String getConversionResult() {
+        return conversionResult;
+    }
+
+    public void setConversionResult(String conversionResult) {
+        this.conversionResult = conversionResult;
+    }
+
+
+
+
+
+
 
     public CurrencyCodes getCurrencyCodes() {
         return currencyCodes;
@@ -15,14 +43,52 @@ public class CurrencyConverter {
         this.currencyCodes = currencyCodes;
     }
 
-    public CurrencyConverter()
-    {
+    public CurrencyConverter() {
         this.currencyCodes = new CurrencyCodes();
         currencyCodes.uploadCodes();
-        this.fromCurrency="EUR";
-        this.toCurrency="RON";
+        this.fromCurrency = "EUR";
+        this.toCurrency = "RON";
+        this.amount = 1;
+        this.conversionResult="";
+        convertCurrency();
     }
 
+    public void convertCurrency() {
+
+        String url_str = "https://v6.exchangerate-api.com/v6/" + DotenvLoader.getApiKey() + "/pair/" + fromCurrency + "/" + toCurrency + "/" + amount;
+
+        try {
+            URL url = new URL(url_str);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            int code= request.getResponseCode();
+            if(code==403)
+            {
+                this.conversionResult="API key is invalid or has expired";
+            }
+
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
+
+            String req_result = jsonobj.get("result").getAsString();
+
+                this.conversionRate = Double.parseDouble(jsonobj.get("conversion_rate").getAsString());
+                this.convertedAmount = Double.parseDouble(jsonobj.get("conversion_result").getAsString());
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            System.out.println("Error in CurrencyConverter.convertCurrency()");
+            e.printStackTrace();
+        }
+
+
+    }
 
     public String getFromCurrency() {
         return fromCurrency;
@@ -55,7 +121,13 @@ public class CurrencyConverter {
     public void setConvertedAmount(double convertedAmount) {
         this.convertedAmount = convertedAmount;
     }
-    private void convertCurrency() {
-        
+
+
+    public double getConversionRate() {
+        return conversionRate;
+    }
+
+    public void setConversionRate(double conversionRate) {
+        this.conversionRate = conversionRate;
     }
 }
