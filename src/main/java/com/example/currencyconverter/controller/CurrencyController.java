3ee0a434c541d4cd7ca2a5db11299d764a594bc3
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-27 12:34:50 +0300

    CurrencyController created

---
+++ src/main/java/com/example/currencyconverter/controller/CurrencyController.java
@@ -0,0 +1,12 @@
+package com.example.currencyconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CurrencyController {
+    @GetMapping("/")
+    public String getCurrencyConverter() {
+        return "currency";
+    }
+}
