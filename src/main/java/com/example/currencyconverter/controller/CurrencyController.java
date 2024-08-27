1757a31a252944ba2a0c8fd9b01d0263c424f2df
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-27 15:56:30 +0300

    model atributes added

--- src/main/java/com/example/currencyconverter/controller/CurrencyController.java
+++ src/main/java/com/example/currencyconverter/controller/CurrencyController.java
@@ -13,19 +13,24 @@ public class CurrencyController {
     @GetMapping("/")
     public String getCurrencyConverter(Model model) {
         CurrencyConverter currencyConverter = new CurrencyConverter();
-        System.out.println( currencyConverter.getCurrencyCodes().toString());
+
+
         model.addAttribute("currencyConverter", currencyConverter);
         model.addAttribute("currencyCodes", currencyConverter.getCurrencyCodes().getCurrencyCodes());
         model.addAttribute("fromCurrency",currencyConverter.getFromCurrency());
         model.addAttribute("toCurrency",currencyConverter.getToCurrency());
+        model.addAttribute("conversionResult",currencyConverter.getConversionResult());
 
         return "currency";
     }
     @PostMapping("/")
     public String convertCurrency(CurrencyConverter currencyConverter, Model model) {
-//        currencyConverter.convertCurrency();
+        currencyConverter.convertCurrency();
         model.addAttribute("currencyConverter", currencyConverter);
         model.addAttribute("currencyCodes", currencyConverter.getCurrencyCodes().getCurrencyCodes());
+        model.addAttribute("fromCurrency",currencyConverter.getFromCurrency());
+        model.addAttribute("toCurrency",currencyConverter.getToCurrency());
+        model.addAttribute("conversionResult",currencyConverter.getConversionResult());
         return "currency";
     }
 }
