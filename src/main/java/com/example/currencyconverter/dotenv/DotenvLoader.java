f5236d13b45d2f8fddf39adf01371c4dd1c8f36c
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-27 15:59:18 +0300

    dependency for Gson library added

--- src/main/java/com/example/currencyconverter/dotenv/DotenvLoader.java
+++ src/main/java/com/example/currencyconverter/dotenv/DotenvLoader.java
@@ -5,6 +5,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DotenvLoader {
     public static final Dotenv dotenv = Dotenv.load();
     public static String getApiKey(){
+        if(dotenv.get("API_KEY")==null){
+            throw new RuntimeException("API_KEY not found in .env file");
+        }
         return dotenv.get("API_KEY");
     }
 }
